d72882fe3b24c2bafe27a0ed431f96c5093d0bb1
Karn Wong <karn@karnwong.me>
2024-03-04 21:00:42 +0700

    set sidebar for apps

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,16 +5,8 @@
     target="_blank"
     :href="link"
   >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
-      <q-icon :name="icon" />
-    </q-item-section>
-
     <q-item-section>
       <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
@@ -23,27 +15,16 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'EssentialLink',
+  name: 'App',
   props: {
     title: {
       type: String,
       required: true
     },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
     link: {
       type: String,
       default: '#'
     },
-
-    icon: {
-      type: String,
-      default: ''
-    }
   }
 });
 </script>
