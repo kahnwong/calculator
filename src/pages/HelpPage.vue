b9f8d11df438c4b0f35d2f7d03278040b9bd7e2a
Karn Wong <karn@karnwong.me>
2024-03-06 09:25:02 +0700

    add text input

--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,7 +1,25 @@
 <template>
-  <q-page>
-    <div class="q-pa-md">
-      <p>Help</p>
+  <q-page class="q-pa-lg">
+    <div class="text-h2 text-bold">Meeting Cost Calculator</div>
+
+    <div class="text-h4 text-bold">How much does this meeting really cost?</div>
+
+    <div class="row">
+      <div class="col-6">
+        <q-input outlined>
+          <template v-slot:append>
+            <div class="row">
+              <div class="col-6">
+                <q-icon name="fa-solid fa-plus" />
+              </div>
+              <div class="col-6">
+                <q-icon name="fa-solid fa-minus" />
+              </div>
+            </div>
+          </template>
+        </q-input>
+      </div>
+      <div class="col-6">a b c</div>
     </div>
   </q-page>
 </template>
