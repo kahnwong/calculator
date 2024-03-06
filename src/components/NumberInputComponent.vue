c368d1de05d552a34108d376fa2912f7a4bd52f9
Karn Wong <karn@karnwong.me>
2024-03-06 09:55:38 +0700

    refactor to component

---
+++ src/components/NumberInputComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-input
+    outlined
+    dense
+    :model-value="modelValue"
+    @update:model-value="(value) => $emit('update:modelValue', value)"
+  >
+    <template v-slot:append>
+      <div class="row justify-center items-center text-center full-height">
+        <div
+          class="col-6 row justify-center items-center text-center full-height q-px-sm"
+          :class="bgPlusActive ? 'bg-blue' : ''"
+          @mouseover="bgPlusActive = true"
+          @mouseleave="bgPlusActive = false"
+          @click="$emit('update:modelValue', modelValue + 5)"
+        >
+          <q-icon name="fa-solid fa-plus" class="cursor-pointer" />
+        </div>
+        <div
+          class="col-6 row justify-center items-center text-center full-height q-px-sm"
+          :class="bgMinusActive ? 'bg-blue' : ''"
+          @mouseover="bgMinusActive = true"
+          @mouseleave="bgMinusActive = false"
+          @click="$emit('update:modelValue', modelValue - 5)"
+        >
+          <q-icon name="fa-solid fa-minus" class="cursor-pointer" />
+        </div>
+      </div>
+    </template>
+  </q-input>
+</template>
+<script lang="ts">
+export default {
+  name: 'NumberInputComponent',
+  props: {
+    modelValue: {},
+  },
+  data() {
+    return {
+      bgPlusActive: false,
+      bgMinusActive: false,
+    };
+  },
+};
+</script>
--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -6,32 +6,7 @@
 
     <div class="row">
       <div class="col-6">
-        <q-input outlined dense v-model.number="attendees">
-          <template v-slot:append>
-            <div
-              class="row justify-center items-center text-center full-height"
-            >
-              <div
-                class="col-6 row justify-center items-center text-center full-height q-px-sm"
-                :class="bgPlusActive ? 'bg-blue' : ''"
-                @mouseover="bgPlusActive = true"
-                @mouseleave="bgPlusActive = false"
-                @click="attendees += 5"
-              >
-                <q-icon name="fa-solid fa-plus" class="cursor-pointer" />
-              </div>
-              <div
-                class="col-6 row justify-center items-center text-center full-height q-px-sm"
-                :class="bgMinusActive ? 'bg-blue' : ''"
-                @mouseover="bgMinusActive = true"
-                @mouseleave="bgMinusActive = false"
-                @click="attendees -= 5"
-              >
-                <q-icon name="fa-solid fa-minus" class="cursor-pointer" />
-              </div>
-            </div>
-          </template>
-        </q-input>
+        <NumberInputComponent v-model.number="attendees" />
       </div>
       <div class="col-6">a b c</div>
     </div>
@@ -40,15 +15,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import NumberInputComponent from 'components/NumberInputComponent.vue';
+
 export default defineComponent({
   name: 'HelpPage',
+  components: { NumberInputComponent },
   data() {
     return {
-      // button
-      bgPlusActive: false,
-      bgMinusActive: false,
-
-      // main
       attendees: 5,
     };
   },
