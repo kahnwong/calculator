c30d1aac263456b7565154a9fe196ec433aae099
Karn Wong <karn@karnwong.me>
2024-03-07 15:31:21 +0700

    move GKEModel to separate file

---
+++ src/constants/GKEModel.ts
@@ -0,0 +1,39 @@
+// pricing
+// """
+// Ref: https://cloud.google.com/kubernetes-engine/pricing
+// Unit: USD
+// Price: per hour
+// Region: asia-southeast1
+// """
+export const generalPurpose = {
+  cpu_regular: 0.0549,
+  memory_regular: 0.0060729,
+  cpu_spot: 0.0165,
+  memory_spot: 0.0018219,
+  cpu_one_year_commitment: 0.04392,
+  memory_one_year_commitment: 0.0048583,
+  cpu_three_year_commitment: 0.030195,
+  memory_three_year_commitment: 0.0033401,
+};
+
+export const scaleOutARM = {
+  cpu_regular: 0.0439,
+  memory_regular: 0.0048583,
+  cpu_spot: 0.0132,
+  memory_spot: 0.0014575,
+  cpu_one_year_commitment: 0.03512,
+  memory_one_year_commitment: 0.0038866,
+  cpu_three_year_commitment: 0.024145,
+  memory_three_year_commitment: 0.0026721,
+};
+
+export const scaleOutX86 = {
+  cpu_regular: 0.0692,
+  memory_regular: 0.0076518,
+  cpu_spot: 0.0208,
+  memory_spot: 0.0022956,
+  cpu_one_year_commitment: 0.05536,
+  memory_one_year_commitment: 0.00612144,
+  cpu_three_year_commitment: 0.03806,
+  memory_three_year_commitment: 0.0042085,
+};
--- src/pages/GKEAutopilot.vue
+++ src/pages/GKEAutopilot.vue
@@ -43,6 +43,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NumberInputComponent from 'components/NumberInputComponent.vue';
+import {
+  generalPurpose,
+  scaleOutARM,
+  scaleOutX86,
+} from 'src/constants/GKEModel';
 
 export default defineComponent({
   name: 'HelpPage',
@@ -64,46 +69,6 @@ export default defineComponent({
   method: {},
   computed: {
     finalValue() {
-      // pricing
-      // """
-      // Ref: https://cloud.google.com/kubernetes-engine/pricing
-      // Unit: USD
-      // Price: per hour
-      // Region: asia-southeast1
-      // """
-      let generalPurpose = {
-        cpu_regular: 0.0549,
-        memory_regular: 0.0060729,
-        cpu_spot: 0.0165,
-        memory_spot: 0.0018219,
-        cpu_one_year_commitment: 0.04392,
-        memory_one_year_commitment: 0.0048583,
-        cpu_three_year_commitment: 0.030195,
-        memory_three_year_commitment: 0.0033401,
-      };
-
-      let scaleOutARM = {
-        cpu_regular: 0.0439,
-        memory_regular: 0.0048583,
-        cpu_spot: 0.0132,
-        memory_spot: 0.0014575,
-        cpu_one_year_commitment: 0.03512,
-        memory_one_year_commitment: 0.0038866,
-        cpu_three_year_commitment: 0.024145,
-        memory_three_year_commitment: 0.0026721,
-      };
-
-      let scaleOutX86 = {
-        cpu_regular: 0.0692,
-        memory_regular: 0.0076518,
-        cpu_spot: 0.0208,
-        memory_spot: 0.0022956,
-        cpu_one_year_commitment: 0.05536,
-        memory_one_year_commitment: 0.00612144,
-        cpu_three_year_commitment: 0.03806,
-        memory_three_year_commitment: 0.0042085,
-      };
-
       // base price
       let generalPurposeRegularPerHour =
         this.vCPU.value * generalPurpose.cpu_regular +
