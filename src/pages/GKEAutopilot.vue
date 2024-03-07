2b38d9397d5e4daa482088b3658aea1a3647b8a7
Karn Wong <karn@karnwong.me>
2024-03-07 14:12:55 +0700

    add price

--- src/pages/GKEAutopilot.vue
+++ src/pages/GKEAutopilot.vue
@@ -9,11 +9,11 @@
     <div class="row">
       <div class="col-4">
         <NumberInputComponent
-          v-model.number="cpu.value"
-          :increment-value="cpu.increment"
-          :min-value="cpu.min"
+          v-model.number="vCPU.value"
+          :increment-value="vCPU.increment"
+          :min-value="vCPU.min"
           label="vCPU"
-          :rules="[(val) => val >= cpu.min || `Minimum value is ${cpu.min}`]"
+          :rules="[(val) => val >= vCPU.min || `Minimum value is ${vCPU.min}`]"
         />
         <NumberInputComponent
           v-model.number="memory.value"
@@ -48,7 +48,7 @@ export default defineComponent({
   components: { NumberInputComponent },
   data() {
     return {
-      cpu: {
+      vCPU: {
         value: 0.25,
         increment: 1,
         min: 0.25,
@@ -63,6 +63,46 @@ export default defineComponent({
   method: {},
   computed: {
     finalValue() {
+      // pricing
+      // """
+      // Ref: https://cloud.google.com/kubernetes-engine/pricing
+      // Unit: USD
+      // Price: per hour
+      // Region: asia-southeast1
+      // """
+      let generalPurpose = {
+        cpu_regular: 0.0549,
+        memory_regular: 0.0060729,
+        cpu_spot: 0.0165,
+        memory_spot: 0.0018219,
+        cpu_one_year_commitment: 0.04392,
+        memory_one_year_commitment: 0.0048583,
+        cpu_three_year_commitment: 0.030195,
+        memory_three_year_commitment: 0.0033401,
+      };
+
+      // base price
+      function perHourToPerMonth(perHour: number) {
+        return perHour * 24 * 30;
+      }
+
+      let generalPurposeRegularPerHour =
+        this.vCPU.value * generalPurpose.cpu_regular +
+        this.memory.value * generalPurpose.memory_regular;
+      let generalPurposeSpotPerHour =
+        this.vCPU.value * generalPurpose.cpu_spot +
+        this.memory.value * generalPurpose.memory_spot;
+      let generalPurposeOneYearCommitmentPerHour =
+        this.vCPU.value * generalPurpose.cpu_one_year_commitment +
+        this.memory.value * generalPurpose.memory_one_year_commitment;
+      let generalPurposeThreeYearCommitmentPerHour =
+        this.vCPU.value * generalPurpose.cpu_three_year_commitment +
+        this.memory.value * generalPurpose.memory_three_year_commitment;
+
+      // constant
+      const fractionDigits = 3;
+
+      // table data
       let columns = [
         {
           name: 'name',
@@ -96,10 +136,38 @@ export default defineComponent({
 
       let rows = [
         {
-          name: 'aaaa',
-          pricing: 'a',
-          pricePerHour: 159,
-          pricePerMonth: 159,
+          name: 'General Purpose',
+          pricing: 'Regular',
+          pricePerHour: generalPurposeRegularPerHour.toFixed(fractionDigits),
+          pricePerMonth: perHourToPerMonth(
+            generalPurposeRegularPerHour
+          ).toFixed(fractionDigits),
+        },
+        {
+          name: 'General Purpose',
+          pricing: 'Spot',
+          pricePerHour: generalPurposeSpotPerHour.toFixed(fractionDigits),
+          pricePerMonth: perHourToPerMonth(generalPurposeSpotPerHour).toFixed(
+            fractionDigits
+          ),
+        },
+        {
+          name: 'General Purpose',
+          pricing: '1 Year Commitment',
+          pricePerHour:
+            generalPurposeOneYearCommitmentPerHour.toFixed(fractionDigits),
+          pricePerMonth: perHourToPerMonth(
+            generalPurposeOneYearCommitmentPerHour
+          ).toFixed(fractionDigits),
+        },
+        {
+          name: 'General Purpose',
+          pricing: '3 Year Commitment',
+          pricePerHour:
+            generalPurposeThreeYearCommitmentPerHour.toFixed(fractionDigits),
+          pricePerMonth: perHourToPerMonth(
+            generalPurposeThreeYearCommitmentPerHour
+          ).toFixed(fractionDigits),
         },
       ];
 
