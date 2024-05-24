823780e1cb6dd04632d7886f38d540618180937e
Karn Wong <karn@karnwong.me>
2024-05-24 17:01:36 +0700

    refactor spark job to data class

--- src/pages/GKEAutopilotSparkJob.vue
+++ src/pages/GKEAutopilotSparkJob.vue
@@ -8,49 +8,24 @@
     <div class="q-pb-md"></div>
     <div class="row">
       <div class="col-4">
-        <NumberInputComponent
-          v-model.number="vCPU.value"
-          :increment-value="vCPU.increment"
-          :min-value="vCPU.min"
-          label="vCPU"
-          :rules="[(val) => val >= vCPU.min || `Minimum value is ${vCPU.min}`]"
-        />
-        <NumberInputComponent
-          v-model.number="memory.value"
-          :increment-value="memory.increment"
-          :min-value="memory.min"
-          label="Memory"
-          :rules="[
-            (val) => val >= memory.min || `Minimum value is ${memory.min}`,
-          ]"
-        />
-        <NumberInputComponent
-          v-model.number="executors.value"
-          :increment-value="executors.increment"
-          :min-value="executors.min"
-          label="Executors"
-          :rules="[
-            (val) =>
-              val >= executors.min || `Minimum value is ${executors.min}`,
-          ]"
-        />
-        <NumberInputComponent
-          v-model.number="jobDurationHour.value"
-          :increment-value="jobDurationHour.increment"
-          :min-value="jobDurationHour.min"
-          label="Job duration (hours)"
-          :rules="[
-            (val) =>
-              val >= jobDurationHour.min ||
-              `Minimum value is ${jobDurationHour.min}`,
-          ]"
-        />
+        <div v-for="(value, name) in gkeAutopilotSparkJob" :key="name">
+          <NumberInputComponent
+            v-model.number="value.value"
+            :increment-value="value.increment"
+            :min-value="value.min"
+            :max-value="value.max"
+            :label="value.label"
+            :rules="[
+              (val) => val >= value.min || `Minimum value is ${value.min}`,
+              (val) => val <= value.max || `Maximum value is ${value.max}`,
+            ]"
+          />
+        </div>
       </div>
       <div class="col-1"></div>
       <div class="col-4 q-pl-lg">
         <div class="text-h4 text-bold">${{ finalValue.regularSpotPrice }}</div>
         <div class="q-pb-xl">Regular Spot Price</div>
-
         <div class="text-h4 text-bold">
           ${{ finalValue.scaleOutARMSpotPrice }}
         </div>
@@ -64,58 +39,66 @@
 import { defineComponent } from 'vue';
 import NumberInputComponent from 'components/NumberInputComponent.vue';
 import { generalPurposeModel, scaleOutARMModel } from 'src/models/GcpGkeModel';
+import {
+  sparkGeneralPurposeModel,
+  sparkScaleOutARMModel,
+} from 'src/models/GcpGkeSparkJobModel';
 
 export default defineComponent({
   name: 'HelpPage',
   components: { NumberInputComponent },
   data() {
     return {
-      vCPU: {
-        value: 0.25,
-        increment: 1,
-        min: 0.25,
-      },
-      memory: {
-        value: 0.5,
-        increment: 2,
-        min: 0.5,
-      },
-      executors: {
-        value: 1,
-        increment: 1,
-        min: 1,
-      },
-      jobDurationHour: {
-        value: 1,
-        increment: 2,
-        min: 0.5,
+      gkeAutopilotSparkJob: {
+        vCPU: {
+          label: 'vCPU',
+          value: 0.25,
+          increment: 1,
+          min: 0.25,
+        },
+        memory: {
+          label: 'Memory',
+          value: 0.5,
+          increment: 2,
+          min: 0.5,
+        },
+        executors: {
+          label: 'Executors',
+          value: 1,
+          increment: 1,
+          min: 1,
+        },
+        jobDurationHour: {
+          label: 'Job duration (hours)',
+          value: 1,
+          increment: 2,
+          min: 0.5,
+        },
       },
     };
   },
   method: {},
   computed: {
     finalValue() {
-      // regular spot
-      let regularSpotPricePerHour =
-        this.vCPU.value * generalPurposeModel.cpu_spot +
-        this.memory.value * generalPurposeModel.memory_spot;
-      let regularSpotPrice =
-        regularSpotPricePerHour *
-        this.jobDurationHour.value *
-        (1 + this.executors.value);
+      let regular = new sparkGeneralPurposeModel(
+        this.gkeAutopilotSparkJob.vCPU.value,
+        this.gkeAutopilotSparkJob.memory.value,
+        this.gkeAutopilotSparkJob.executors.value,
+        this.gkeAutopilotSparkJob.jobDurationHour.value,
+      );
+      let regularSpotPrice = regular.costSpotJob();
 
-      // scale out arm spot
-      let scaleOutARMSpotPricePerHour =
-        this.vCPU.value * scaleOutARMModel.cpu_spot +
-        this.memory.value * scaleOutARMModel.memory_spot;
-      let scaleOutARMSpotPrice =
-        scaleOutARMSpotPricePerHour *
-        this.jobDurationHour.value *
-        (1 + this.executors.value);
+      let arm = new sparkScaleOutARMModel(
+        this.gkeAutopilotSparkJob.vCPU.value,
+        this.gkeAutopilotSparkJob.memory.value,
+        this.gkeAutopilotSparkJob.executors.value,
+        this.gkeAutopilotSparkJob.jobDurationHour.value,
+      );
+      let armSpotPrice = arm.costSpotJob();
 
       return {
         regularSpotPrice: regularSpotPrice,
-        scaleOutARMSpotPrice: scaleOutARMSpotPrice,
+        scaleOutARMSpotPrice: armSpotPrice,
       };
     },
   },
