c4574ddfa5b1b611693233a331c7da5f68d3d0e5
Karn Wong <karn@karnwong.me>
2025-01-06 10:43:17 +0700

    move script tag to main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,14 @@
 <template>
   <q-layout view="hHh Lpr lFf">
+    <!--    header-->
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title> Calculator </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
+    <!--    sidebar-->
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered :breakpoint="500" :width="200">
       <q-tabs vertical align="left" dense>
         <q-item-label header class="text-bold">
@@ -17,7 +18,6 @@
         <AppLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-tabs>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -56,4 +56,11 @@ const leftDrawerOpen = ref(false)
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
+
+// analytics
+const analyticsScript = document.createElement('script')
+analyticsScript.setAttribute('defer', '')
+analyticsScript.setAttribute('src', 'https://umami.karnwong.me/script.js')
+analyticsScript.setAttribute('data-website-id', 'de8b4631-aac2-41ef-9286-38730945ba04')
+document.head.appendChild(analyticsScript)
 </script>
